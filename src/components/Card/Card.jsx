af4808d1183f8d034040d32ac7d4ba236fe944f4
TomCostabel <tomasperalta1997@hotmail.com>
2022-10-03 17:19:42 -0300

    Ecommerce

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "../Card/Card.css";
 
 export default function Card(props) {
     return (
-        <div>
+        <div className="container-card">
+            <img className="imagen" src={props.images[0]} alt="imagencard" />
             <h3>{props.title}</h3>
+            <h4>{props.price}</h4>
         </div>
     );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllProducts } from "../../redux/actions";
+import Card from "../Card/Card";
+import "../Home/Home.css";
 
 export default function Home() {
     const products = useSelector((state) => state.productos);
@@ -8,10 +10,24 @@ export default function Home() {
     useEffect(() => {
         dispatch(getAllProducts());
     }, [dispatch]);
+
     console.log("estos son los productos", products);
+
+    const allProducts = products?.map((e) => {
+        return (
+            <Card
+                key={e.id}
+                id={e.id}
+                title={e.title}
+                images={e.images}
+                price={e.price}
+            />
+        );
+    });
+    console.log("acaaa", allProducts);
     return (
         <div>
-            <h1>Hola</h1>
+            <h3 className="container-cards">{allProducts}</h3>
         </div>
     );
 }
