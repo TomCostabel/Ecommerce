59ceeefe19fe2f8f138d45ee7521318a35f6b4ee
TomCostabel <tomasperalta1997@hotmail.com>
2022-11-22 17:09:55 -0300

    Sumar producto al LocalStorage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,13 +5,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllProducts } from "../../redux/actions";
 
 import "../Cart/Cart.css";
-// let localStorageCart = JSON.parse(localStorage.getItem("carrito")) || [];
 
 export default function Carrito() {
     const dispatch = useDispatch();
     const products = useSelector((state) => state.productos);
 
     const [cart, setCart] = useState([]);
+
     useEffect(() => {
         dispatch(getAllProducts());
         let changuito2 = JSON.parse(localStorage.getItem("carrito")) || [];
@@ -32,7 +32,24 @@ export default function Carrito() {
             {products?.map((e) => {
                 var count = 0;
 
-                if (cart?.includes(e.title))
+                if (cart?.includes(e.title)) {
+                    //-------------------------------------- RESTAR PRODUCTOS AL LocalStorage ------------------//
+
+                    const restarProducto = (value) => {
+                        let restProduct = cart.find((e) => e === value);
+                        console.log(restProduct);
+                    };
+                    //-------------------------------------- SUMAR PRODUCTOS AL LocalStorage ------------------//
+
+                    const sumarProducto = (value) => {
+                        let newVariable = [...cart, value];
+                        setCart(newVariable);
+                        localStorage.setItem(
+                            "carrito",
+                            JSON.stringify(newVariable)
+                        );
+                    };
+
                     return (
                         <div key={e.id}>
                             <div className="container-cada-producto">
@@ -52,14 +69,22 @@ export default function Carrito() {
                                               totalCantidad + e.price)
                                         : count
                                 )}
+                                <button onClick={() => restarProducto(e.title)}>
+                                    -
+                                </button>
 
                                 <h3>{count} </h3>
+                                <button onClick={() => sumarProducto(e.title)}>
+                                    +
+                                </button>
+
                                 <h5>{e.title}</h5>
 
                                 <p>US$ {e.price}</p>
                             </div>
                         </div>
                     );
+                }
             })}
 
             <h1>Total ${totalCantidad}</h1>
